0789bd8eb5d45b0114e5036c8ccaad9e568f4854
Mohammed Ramzy <agr.MohamedRamzy230906@alexu.edu.eg>
2025-02-26 14:53:45 +0200

    test4

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,50 @@
+// import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+// import { provideRouter } from '@angular/router';
+
+// import { routes } from './app.routes';
+// import {
+//   provideClientHydration,
+//   withEventReplay,
+// } from '@angular/platform-browser';
+// import { HttpClient, provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+// import { provideAnimations } from '@angular/platform-browser/animations';
+// import { provideToastr } from 'ngx-toastr';
+// import { headersInterceptor } from './core/interceptors/headers/headers.interceptor';
+// import { errorsInterceptor } from './core/interceptors/errors/errors.interceptor';
+// import { NgxSpinnerModule } from "ngx-spinner";
+// import { loadingInterceptor } from './core/interceptors/loading/loading.interceptor';
+// import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+// import { HttpLoaderFactory } from './core/utils/httploadfile';
+
+
+
+
+// export const appConfig: ApplicationConfig = {
+//   providers: [
+//     provideZoneChangeDetection({ eventCoalescing: true }),
+//     provideRouter(routes),
+//     provideClientHydration(withEventReplay()),
+//     provideHttpClient(withFetch(), withInterceptors([headersInterceptor  ,  errorsInterceptor , loadingInterceptor])),
+//     provideAnimations(),
+//     provideToastr(),
+//     importProvidersFrom(NgxSpinnerModule , TranslateModule.forRoot({
+//       defaultLanguage:'en',
+//       loader:{
+//         provide:TranslateLoader,
+//         useFactory:HttpLoaderFactory,
+//         deps:[HttpClient]
+//       }
+//     }))
+//   ],
+// };
+
+
+
+
+
+
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import {
@@ -16,24 +61,24 @@ import { loadingInterceptor } from './core/interceptors/loading/loading.intercep
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { HttpLoaderFactory } from './core/utils/httploadfile';
 
-
-
-
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()), // ✅ تفعيل HashLocationStrategy
     provideClientHydration(withEventReplay()),
-    provideHttpClient(withFetch(), withInterceptors([headersInterceptor  ,  errorsInterceptor , loadingInterceptor])),
+    provideHttpClient(withFetch(), withInterceptors([headersInterceptor, errorsInterceptor, loadingInterceptor])),
     provideAnimations(),
     provideToastr(),
-    importProvidersFrom(NgxSpinnerModule , TranslateModule.forRoot({
-      defaultLanguage:'en',
-      loader:{
-        provide:TranslateLoader,
-        useFactory:HttpLoaderFactory,
-        deps:[HttpClient]
-      }
-    }))
+    importProvidersFrom(
+      NgxSpinnerModule,
+      TranslateModule.forRoot({
+        defaultLanguage: 'en',
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient],
+        },
+      })
+    ),
   ],
 };
